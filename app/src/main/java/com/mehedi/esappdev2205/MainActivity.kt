cd06a9697abd4f87da1e197a5dfedad7630333ec
Mehedi <mehedi.hasan.app.dev@gmail.com>
2023-03-02 19:57:29 +0600

    class 13 widgets

--- app/src/main/java/com/mehedi/esappdev2205/MainActivity.kt
+++ app/src/main/java/com/mehedi/esappdev2205/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mehedi.esappdev2205
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
 import android.widget.Toast
 import com.mehedi.esappdev2205.databinding.ActivityMainBinding
 
@@ -14,6 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.modeSwitch.setOnCheckedChangeListener { _, isChecked ->
+
+            if (isChecked) {
+                Toast.makeText(this, " agreeCheckBox Checked", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "agreeCheckBox Not Checked", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
+        binding.agreeCheckBox.setOnCheckedChangeListener { _, isChecked ->
+
+            if (!isChecked) {
+                Toast.makeText(this, "modeSwitch Checked", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "modeSwitch Not Checked", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
 
 
     }
