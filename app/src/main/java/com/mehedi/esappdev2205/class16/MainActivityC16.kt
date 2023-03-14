f3e08922dd451a0cd1611c7db31a7b7a2f15fdbf
Mehedi <mehedi.hasan.app.dev@gmail.com>
2023-03-14 18:44:25 +0600

    class 16 initial commit

---
+++ app/src/main/java/com/mehedi/esappdev2205/class16/MainActivityC16.kt
@@ -0,0 +1,12 @@
+package com.mehedi.esappdev2205.class16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mehedi.esappdev2205.R
+
+class MainActivityC16 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_c16)
+    }
+}
